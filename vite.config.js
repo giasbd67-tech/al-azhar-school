ec62ae6c16ab24e738ff42228fb02eeb725d34ee
giasbd67-tech <giasbd67@gmail.com>
2026-01-21 03:45:57 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // এটি সাদা স্ক্রিন সমস্যা সমাধান করবে
   build: {
     outDir: 'dist',
     rollupOptions: {
@@ -12,4 +13,7 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    historyApiFallback: true,
+  }
 })
