21cc27aeb9ae03abeaffef722b93e0c34707ed74
giasbd67-tech <giasbd67@gmail.com>
2026-01-23 03:22:42 +0600

    Update main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// './index.css' পরিবর্তন করে আপনার ফাইলের নাম './style.css' দেওয়া হয়েছে
+import './style.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
