5d88a84849ad228327cc6075e96a55fd936b5981
giasbd67-tech <giasbd67@gmail.com>
2026-01-22 18:38:08 +0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -6,8 +6,8 @@ app.use(express.json());
 
 registerRoutes(app);
 
-// এখানে 'precass' এর বদলে 'process' হবে
-const PORT = process.env.PORT || 5000;
+// process বানানটি এখানে সঠিক করা হয়েছে
+const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
