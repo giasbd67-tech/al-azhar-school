c56cb68425106611e4fb9905c84ddc7408e70f73
giasbd67-tech <giasbd67@gmail.com>
2026-01-24 10:57:20 +0600

    Delete shared directory

--- shared/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, text, serial, integer } from "drizzle-orm/pg-core";
-
-export const students = pgTable("students", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  class: text("class").notNull(),
-  rollNo: integer("roll_no").notNull(),
-});
